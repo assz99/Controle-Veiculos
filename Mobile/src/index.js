34eaab8eabf07aa2cf7791bfa22f020363156e54
Renan <renanguides@hotmail.com>
2021-01-03 02:20:15 -0400

    Adicionado a função de se o usuario ja foi logado
    Adicionado funçao para verificar se o usuario ja logou alguma vez

--- Mobile/src/index.js
+++ Mobile/src/index.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useReducer, useState, Component } from 'react';
-import { StyleSheet, Text, View, Button, Image, TextInput, } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, TextInput, Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import api from './services/api'
@@ -10,9 +10,9 @@ function clickou() {
   console.log("CLickou no botao")
 }
 
-const params ={
-   "email": "renanguides@hotmaial.com",
-    "password": "123456"
+const params = {
+  "email": "renanguides@hotmail.com",
+  "password": "123456"
 }
 
 var error = "";
@@ -20,64 +20,75 @@ var error = "";
 export default class App extends Component {
   state = {
     error: '',
-    //errorMessage: '',
+    loggedInUser: null,
   }
 
-  
-  
+
+
   signIn = async () => {
 
     try {
-      const response = await api.post('/api/authenticate', 
-      params);
+      const response = await api.post('/api/authenticate',
+        params);
       console.log("Clickou no botao");
 
-      const { token, user } = response.data;
-      
-      
+      const { user, token } = response.data;
 
       await AsyncStorage.multiSet([
         ['@CodeApi:token', token],
         ['@CodeApi:token', JSON.stringify(user)]
       ]);
+
+      this.setState({ loggedInUser: user.name });
+
+      Alert.alert('Login com Sucesso!');
+
     } catch (response) {
-      this.setState({ error:response.data.error});
-      
+      this.setState({ error: response.data.error });
+
       error = JSON.stringify(this.state.error);
-    
-      console.log(typeof( error));
+
+      console.log(typeof (error));
     };
-
-
   };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Image style={styles.logo}
-          source={img_Logo}
-        />
-        <TextInput
-          style={{ height: 40, width: 220, borderColor: 'white', borderWidth: 1, color: 'white' }}
-          placeholder="                     Usuário"
-          placeholderTextColor="#FFFFFF"
+  async componentDidMount() {
+    const token = await AsyncStorage.getItem('@CodeApi:token');
+    const user = JSON.parse(await AsyncStorage.getItem('@CodeApi:user'))
 
-        />
+    if (token && user)
+      this.setState({ loggedInUser: user });
+  }
 
-        <TextInput
-          style={{ height: 40, width: 220, borderColor: 'white', borderWidth: 1, color: 'white', margin: 25 }}
-          placeholder="                       Senha"
-          placeholderTextColor="#FFFFFF"
-        />
 
-        <Button onPress={this.signIn} title="Entrar"></Button>
+render() {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo}
+        source={img_Logo}
+      />
+      <TextInput
+        style={{ height: 40, width: 220, borderColor: 'white', borderWidth: 1, color: 'white' }}
+        placeholder="                     Usuário"
+        placeholderTextColor="#FFFFFF"
 
-        {!!this.state.error && <Text style={styles.White}>{ this.state.error}</Text> }
+      />
 
-        
-      </View>
-    );
-  };
+      <TextInput
+        style={{ height: 40, width: 220, borderColor: 'white', borderWidth: 1, color: 'white', margin: 25 }}
+        placeholder="                       Senha"
+        placeholderTextColor="#FFFFFF"
+      />
+
+      <Button onPress={this.signIn} title="Entrar"></Button>
+
+      {!!this.state.loggedInUser && <Text style={styles.White}>{this.state.loggedInUser}</Text>}
+      {!!this.state.error && <Text style={styles.White}>{this.state.error}</Text>}
+
+
+    </View>
+  );
+};
 }
 
 const styles = StyleSheet.create({
